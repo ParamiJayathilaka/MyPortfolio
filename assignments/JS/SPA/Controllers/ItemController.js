8a584eba38ad0f82ad6d7e09e9d982ed1da27eba
ParamiJayathilaka <thinithiparami53@gmail.com>
2023-10-11 11:08:32 +0530

    'spa'

--- assignments/JS/SPA/Controllers/ItemController.js
+++ assignments/JS/SPA/Controllers/ItemController.js
@@ -1,7 +1,99 @@
-//Write down all the item form controllers here
-
-//load all existing items
 getAllItems();
+
+$("#btnItem").click(function () {
+    if (checkAll()){
+        saveItem();
+    }else{
+        alert("Error");
+    }
+
+});
+
+$("#btnItemGetAll").click(function () {
+    getAllItems();
+});
+
+
+function bindTrEvents() {
+    $('#tblItem>tr').click(function () {
+        //get the selected rows data
+        let code = $(this).children().eq(0).text();
+        let description = $(this).children().eq(1).text();
+        let qty = $(this).children().eq(2).text();
+        let unitPrice = $(this).children().eq(3).text();
+
+
+        //set the selected rows data to the input fields
+        $("#txtItemCode").val(code);
+        $("#txtItemDescription").val(description);
+        $("#txtItemPrice").val(unitPrice);
+        $("#txtItemQty").val(qty);
+
+    })
+}
+
+
+$("#btnItemDelete").click(function () {
+    let code = $("#txtItemCode").val();
+
+    let consent = confirm("Do you want to delete.?");
+    if (consent) {
+        let response = deleteItem(code);
+        if (response) {
+            alert("Item Deleted");
+            clearItemInputFields();
+            getAllItems();
+        } else {
+            alert("Item Not Removed..!");
+        }
+    }
+
+
+});
+
+$("#btnItemUpdate").click(function () {
+    let code = $("#txtItemCode").val();
+    updateItem(code);
+    clearItemInputFields();
+});
+
+$("#btn-clear1").click(function () {
+    clearItemInputFields();
+});
+
+
+function saveItem() {
+    let itemCoad = $("#txtItemCode").val();
+    //check customer is exists or not?
+    if (searchItem(itemCoad.trim()) == undefined) {
+
+        //if the customer is not available then add him to the array
+        let itemCoad = $("#txtItemCode").val();
+        let itemDescription = $("#txtItemDescription").val();
+        let itemQtyOnHand = $("#txtQty").val();
+        let itemUnitPrice = $("#txtItemPrice").val();
+
+        //by using this one we can create a new object using
+        //the customer model with same properties
+        let newItem = Object.assign({}, item);
+
+        //assigning new values for the customer object
+        newItem.code = itemCoad;
+        newItem.description = itemDescription;
+        newItem.qtyOnHand= itemQtyOnHand;
+        newItem.unitPrice = itemUnitPrice;
+
+        //add customer record to the customer array (DB)
+        itemDB.push(newItem);
+        clearItemInputFields();
+        getAllItems();
+
+    } else {
+        alert("Item already exits.!");
+        clearItemInputFields();
+    }
+}
+
 
 
 function getAllItems() {
@@ -28,8 +120,52 @@ function getAllItems() {
         //invoke this method every time
         // we add a row // otherwise click
         //event will not work
+        bindTrEvents();
 
     }
+}
+
+function deleteItem(code) {
+    for (let i = 0; i < itemDB.length; i++) {
+        if (itemDB[i].code == code) {
+            itemDB.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+function searchItem(code) {
+    return itemDB.find(function (item) {
+        //if the search id match with customer record
+        //then return that object
+        return item.code == code;
+    });
+}
+
+function updateItem(code) {
+    if (searchItem(code) == undefined) {
+        alert("No such Item..please check the code");
+    } else {
+        let consent = confirm("Do you really want to update this Item.?");
+        if (consent) {
+            let item = searchItem(code);
+            //if the customer available can we update.?
+
+            let itemCode = $("#txtItemCode").val();
+            let itemDescription = $("#txtItemDescription").val();
+            let itemQtyOnHand = $("#txtQty").val();
+            let itemUnitPrice = $("#txtItemPrice").val();
+
+            item.code = itemCode;
+            item.description = itemDescription;
+            item.qtyOnHand = itemQtyOnHand;
+            item.unitPrice = itemUnitPrice;
+
+            getAllItems();
+        }
+    }
+
 }
 
 
--- assignments/JS/SPA/Controllers/Validation/ItemValidation.js
+++ assignments/JS/SPA/Controllers/Validation/ItemValidation.js
@@ -4,10 +4,10 @@ const ITEM_QTY_REGEX = /^[A-Za-z0-9 ]{8,}$/;
 const ITEM_UNITPRICE_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
 
 let item_vArray = new Array();
-item_vArray.push({field: $("#txtItemCode"), regEx: CUS_ID_REGEX});
-item_vArray.push({field: $("#txtItemDescription"), regEx: CUS_NAME_REGEX});
-item_vArray.push({field: $("#txtItemPrice"), regEx: CUS_ADDRESS_REGEX});
-item_vArray.push({field: $("#txtItemQty"), regEx: CUS_CONTACT_REGEX});
+item_vArray.push({field: $("#txtItemCode"), regEx: ITEM_CODE_REGEX});
+item_vArray.push({field: $("#txtItemDescription"), regEx: ITEM_DESCRIPTION_REGEX});
+item_vArray.push({field: $("#txtItemPrice"), regEx: ITEM_QTY_REGEX});
+item_vArray.push({field: $("#txtItemQty"), regEx: ITEM_UNITPRICE_REGEX});
 
 function clearItemInputFields() {
     $("#txtItemCode,#txtItemDescription,#txtItemPrice,#txtItemQty").val("");
@@ -20,7 +20,7 @@ setBtn();
 
 $("#txtItemCode,#txtItemDescription,#txtItemPrice,#txtItemQty").on("keydown keyup", function (e) {
 
-    let indexNo = c_vArray.indexOf(c_vArray.find((c) => c.field.attr("id") == e.target.id));
+    let indexNo = item_vArray.indexOf(item_vArray.find((c) => c.field.attr("code") == e.target.code));
 
 
     if (e.key == "Tab") {
@@ -28,20 +28,20 @@ $("#txtItemCode,#txtItemDescription,#txtItemPrice,#txtItemQty").on("keydown keyu
     }
 
 
-    checkValidations(c_vArray[indexNo]);
+    checkValidations(item_vArray[indexNo]);
 
     setBtn();
 
     if (e.key == "Enter") {
 
-        if (e.target.id != c_vArray[c_vArray.length - 1].field.attr("id")) {
+        if (e.target.code != item_vArray[item_vArray.length - 1].field.attr("code")) {
 
-            if (checkValidations(c_vArray[indexNo])) {
-                c_vArray[indexNo + 1].field.focus();
+            if (checkValidations(item_vArray[indexNo])) {
+                item_vArray[indexNo + 1].field.focus();
             }
         } else {
-            if (checkValidations(c_vArray[indexNo])) {
-                saveCustomer();
+            if (checkValidations(item_vArray[indexNo])) {
+                saveItem();
             }
         }
     }
@@ -54,4 +54,51 @@ function checkValidations(object) {
     }
     setBorder(false, object)
     return false;
+}
+
+
+
+function setBorder(bol, ob) {
+    if (!bol) {
+        if (ob.field.val().length >= 1) {
+            ob.field.css("border", "2px solid red");
+        } else {
+            ob.field.css("border", "1px solid #ced4da");
+        }
+    } else {
+        if (ob.field.val().length >= 1) {
+            ob.field.css("border", "2px solid green");
+        } else {
+            ob.field.css("border", "1px solid #ced4da");
+        }
+    }
+
+}
+
+function checkAll() {
+    for (let i = 0; i < item_vArray.length; i++) {
+        if (!checkValidations(item_vArray[i])) return false;
+    }
+    return true;
+}
+
+function setBtn() {
+    $("#btnItemDelete").prop("disabled", true);
+    $("#btnItemUpdate").prop("disabled", true);
+
+    if (checkAll()) {
+        $("#btnItem").prop("disabled", false);
+    } else {
+        $("#btnItem").prop("disabled", true);
+    }
+
+    let code = $("#txtItemCode").val();
+    if (searchItem(code) == undefined) {
+        $("#btnItemDelete").prop("disabled", true);
+        $("#btnItemUpdate").prop("disabled", true);
+    } else {
+        $("#btnItemDelete").prop("disabled", false);
+        $("#btnItemUpdate").prop("disabled", false);
+    }
+
 }
