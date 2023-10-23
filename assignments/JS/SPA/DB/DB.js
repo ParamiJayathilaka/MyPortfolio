aa497daa27dff351ef1a0cd11103742505ba2f2a
ParamiJayathilaka <thinithiparami53@gmail.com>
2023-10-23 18:42:23 +0530

    pos

--- assignments/JS/SPA/DB/DB.js
+++ assignments/JS/SPA/DB/DB.js
@@ -1,14 +1,14 @@
 //you can store data like as follows
 var customerDB = [
-    {id: "C00-001", name: "Sachin Thamalsha", address: "Mathara",salary: 100000},
-    {id: "C00-002", name: "Ranjith Perera", address: "Panadura",salary: 200000},
-    {id: "C00-003", name: "Kavindu Perera", address: "Panadura",salary: 300000}
+    {id: "C00-001", name: "Inosha Harshani", address: "Galle",salary: 100000},
+    {id: "C00-002", name: "Gamage Jayathilaka", address: "Hikkaduwa",salary: 200000},
+    {id: "C00-003", name: "Thinithi Parami", address: "Baddegama",salary: 300000}
 ];
 
 var itemDB = [
-    {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
-    {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
-    {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
+    {code:"I00-001",description:"Bluse(XL)",qtyOnHand: 100,unitPrice: 145.00},
+    {code:"I00-002",description:"Shirt",qtyOnHand: 150,unitPrice: 345.00},
+    {code:"I00-003",description:"Frock",qtyOnHand: 400,unitPrice: 245.00}
 ];
 
 var orderDB = [
